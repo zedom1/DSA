37e45035c7a541ea156ba3679b53ceb6936075dc
Zedom <w468750@126.com>
2017-08-11 10:57:08 +0800

    list.cpp

--- list.cpp
+++ list.cpp
@@ -21,9 +21,25 @@ struct ListNode
 	ListNode (T e, ListNode * p = NULL, ListNode *a = NULL)
 		:data(e),pred(p),succ(a){}
 
-	ListNode * insertAsPre (T const & e);
-	ListNode * insertAsAft (T const & e);
+	ListNode * insertAsPred ( T const & e );
+	ListNode * insertAsSucc ( T const & e );
 };
+
+ListNode* ListNode<T>::insertAsPred( T const & e )
+{
+	ListNode *a=new ListNode( e, pred, this );
+	pred->succ=a;
+	pred=a;
+	return a;
+}
+
+ListNode* ListNode<T>::insertAsSucc( T const & e )
+{
+	ListNode *a=new ListNode( e, this, succ );
+	succ->pred=a;
+	succ=a;
+	return a;
+}
 
 template <typename T>
 struct List
@@ -35,20 +51,40 @@ private:
 protected:
 	void init ();
 	int clear ();
-	void copyNode (ListNode *p , int n);
+	void copyNodes ( ListNode *p , int n );     // 从p开始复制n个节点 
 	void merge ( ListNode * &p1, int n1, List<T>&, ListNode *p2, int n2); //归并
-	void mergeSort ( ListNode *&p, int n); //对从p开始连续的n个节点归并排序
-	void selectionSort ( ListNode *p, int n); //对从p开始连续的n个节点选择排序
-	void insertionSort ( ListNode *p, int n); //对从p开始连续的n个节点插入排序
+	void mergeSort ( ListNode *&p, int n);    // 对从p开始连续的n个节点归并排序
+	void selectionSort ( ListNode *p, int n); // 对从p开始连续的n个节点选择排序
+	void insertionSort ( ListNode *p, int n); // 对从p开始连续的n个节点插入排序
 
 public:
-	List() {init();}
+	List() { init(); }
+	List() ( ListNode *p , int n );     // 从p开始复制n个节点 
 	~List();
 
 	// 只读接口
-	T & operator [] (int r) const;
-	ListNode * first() const {return header->succ;}
-	ListNode * last() const {return trailer->pred;}
+	bool empty() { return _size==0; }
+	T & operator [] ( int r ) const;  // 实现寻秩访问
+	ListNode * first() const 
+		{return header->succ;}
+	ListNode * last() const 
+		{return trailer->pred;}
+	ListNode * find( T const&e ) const
+		{return find(e,_size,trailer);}
+	ListNode * find( T const& e, int n, ListNode* p ) const; // 从节点p向前n个节点（不含p）内找e
+
+	// 可写接口
+	ListNode * insertBefore( ListNode *p, T const & e);
+		{ _size++; return p->insertAsPred(e); }
+	ListNode * insertAfter( ListNode *p, T const & e);
+		{ _size++; return p->insertAsSucc(e); }
+	ListNode * insertLast( T const & e )
+		{ _size++; return trailer->insertAsPred(e);}
+	ListNode * insertFirst( T const & e )
+		{ _size++; return header->insertAsSucc(e);}
+	T remove( ListNode *p );
+	int deduplicate (); // 无序列表去重
+	int uniquify (); // 有序列表去重
 };
 
 template <typename T>
@@ -69,4 +105,116 @@ T & List<T>::operator [] (int r)
 	while(r-- && p)
 		p=p->succ;
 	return p->data;
+}
+
+template <typename T>
+ListNode * List<T>::find( T const& e, int n, ListNode* p )
+{
+	while(n--)
+		if( e==( p=p->pred )->data )
+			return p;
+	return NULL;
+}
+
+template <typename T>
+List<T>::List( ListNode * p, int n )
+{
+	init();
+	copyNodes( p , n );
+}
+
+template <typename T>
+void List<T>::copyNodes( ListNode * p, int n )
+{
+/////  方法一：
+// 	ListNode *a=header;
+// 	while(n--)
+// 	{
+// 		a=insertAfter(a,p->data);
+// 		p=p->succ;
+// 	}
+
+/////  方法二：
+	while(n--)
+	{
+		insertLast(p->data);
+		p=p->succ;
+	}
+}
+
+template <typename T>
+T List<T>::remove( ListNode * p )
+{
+	T tem=p->data;
+	p->pred->succ=p->succ;
+	p->succ->pred=p->pred;
+	delete p;
+	_size--;
+	return tem;
+}
+
+template <typename T>
+int List<T> clear()
+{
+// 	ListNode *a=header->succ;
+// 	while(a!=trailer)
+// 	{
+// 		delete a;
+// 		a=header->succ;
+// 	}
+// 	_size=0;
+	int old_size=_size;
+	while( !empty() )
+		remove(first());
+	return old_size;
+}
+
+template <typename T>
+List<T>::~List()
+{
+	clear();
+	delete header;
+	delete trailer;
+}
+
+template <typename T>
+int List<T> deduplicate()
+{
+	ListNode *p=first();
+	int old_size=_size;
+	int r=1;
+	p=p->succ;
+	while( p!=trailer )
+	{
+		if( find( p->data , r , p )!=NULL )
+		{	
+			p=p->succ;
+			remove( p->pred );
+			continue;
+		}
+		r++;
+		p=p->succ;
+	}
+	return old_size-_size;
+}
+
+template <typename T>
+int List<T> uniquify()
+{
+	if(_size<2) return 0;
+	int old_size=_size;
+
+////方法一：
+// 	ListNode *p=header;
+// 	while( trailer!=( p=p->succ ) )
+// 	{
+// 		while( p->data==p->succ->data && p->succ!=trailer )
+// 			remove(p->succ);
+// 	}
+
+////方法二：
+	ListNode *p=first(),*q;
+	while( trailer != ( q = p->succ ) )
+		p->data == q->data ? remove(q) : p=q;
+	return old_size-_size;
 }

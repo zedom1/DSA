ba9a485d79dd08790ba14941230ea064d1a7f50c
Zedom <w468750@126.com>
2017-08-11 14:51:22 +0800

    list.cpp

--- list.cpp
+++ list.cpp
@@ -52,10 +52,10 @@ protected:
 	void init ();
 	int clear ();
 	void copyNodes ( ListNode *p , int n );     // 从p开始复制n个节点 
-	void merge ( ListNode * &p1, int n1, List<T>&, ListNode *p2, int n2); //归并
-	void mergeSort ( ListNode *&p, int n);    // 对从p开始连续的n个节点归并排序
-	void selectionSort ( ListNode *p, int n); // 对从p开始连续的n个节点选择排序
-	void insertionSort ( ListNode *p, int n); // 对从p开始连续的n个节点插入排序
+// 	void merge ( ListNode * &p1, int n1, List<T>&, ListNode *p2, int n2); //归并
+// 	void mergeSort ( ListNode *&p, int n);      // 对从p开始连续的n个节点归并排序
+	void selectionSort ( ListNode *p, int n);   // [ p,p+n ) 选择排序
+	void insertionSort ( ListNode *p, int n);   // 对从p开始连续的n个节点插入排序
 
 public:
 	List() { init(); }
@@ -64,14 +64,16 @@ public:
 
 	// 只读接口
 	bool empty() { return _size==0; }
-	T & operator [] ( int r ) const;  // 实现寻秩访问
+	T & operator [] ( int r ) const;    // 实现寻秩访问
 	ListNode * first() const 
 		{return header->succ;}
 	ListNode * last() const 
 		{return trailer->pred;}
 	ListNode * find( T const&e ) const
 		{return find(e,_size,trailer);}
-	ListNode * find( T const& e, int n, ListNode* p ) const; // 从节点p向前n个节点（不含p）内找e
+	ListNode * find( T const& e, int n, ListNode* p ) const; // (无序)从节点p向前n个节点（不含p）内找e
+	ListNode * search( T const & e , int n , ListNode * p ) const; // (有序) 不大于e的最后一个
+	ListNode * selectMax ( ListNode *p , int n )const ;
 
 	// 可写接口
 	ListNode * insertBefore( ListNode *p, T const & e);
@@ -84,7 +86,7 @@ public:
 		{ _size++; return header->insertAsSucc(e);}
 	T remove( ListNode *p );
 	int deduplicate (); // 无序列表去重
-	int uniquify (); // 有序列表去重
+	int uniquify ();    // 有序列表去重
 };
 
 template <typename T>
@@ -217,4 +219,58 @@ int List<T> uniquify()
 	while( trailer != ( q = p->succ ) )
 		p->data == q->data ? remove(q) : p=q;
 	return old_size-_size;
+}
+
+template <typename T>
+ListNode * List<T>::selectMax ( ListNode *p , int n )
+{
+	ListNode *tem=p;
+	while(n--&&p)
+	{
+		if(p->data>=tem->data)
+			tem=p;
+		p=p->succ;
+	}
+	return tem;
+}
+
+template <typename T>
+void List<T>::selectionSort( ListNode *p, int n )
+{
+	if(n<2) return;
+	ListNode *head=p->pred , *tail=p;
+	for( int i=0; i<n; i++ )
+		tail=tail->succ;
+	while( 1<n )
+	{
+		insertBefore( tail, remove( selectMax( head->succ , n ) ) );
+		tail=tail->pred;
+		n--;
+	}
+	return;
+}
+
+template <typename T>
+ListNode * List<T>::search( T const & e, int n, ListNode * p )
+{
+	while( n--&&p )
+	{
+		if( p->data<=e )
+			break;
+		p=p->pred;
+	}
+	return;
+}
+
+template <typename T>
+void List<T>::insertionSort( ListNode * p , int n )
+{
+	int r=0;
+	while( r<n )
+	{
+		insertAfter( search( p->data , r , p ) , p->data );
+		p=p->succ;
+		remove( p->pred );
+		r++;
+	}
 }
